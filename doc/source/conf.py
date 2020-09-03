54ea6bf70599415488e18830ac802a236ae0e716
luzhenxiong <397132445@qq.com>
2020-09-03 12:59:25 +0800

    a

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -15,7 +15,7 @@ import sys
 sys.path.insert(0, os.path.abspath('..\\..\\SqlTool'))
 ##sys.path.insert(0, os.path.abspath('..\\..\\src'))
 
-#apidoc_module_dir = '..\\..\\src\\新建文件夹'
+apidoc_module_dir = '..\\..\\SqlTool'
 
 # -- Project information -----------------------------------------------------
 
